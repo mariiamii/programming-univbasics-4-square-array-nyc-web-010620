7ee7090a17e6d0d657973c534bcb4812f93ea5b0
Mariam Anuashvili <m.anuashvili@gmail.com>
2019-12-17 03:24:13 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,7 +3,7 @@ def square_array(array)
   counter = 0
   
   while counter < array.length do 
-    array[counter] ** 2
-    squared << counter += 1 
+    squared << (array[counter] ** 2)
+    counter += 1
   end
 end
